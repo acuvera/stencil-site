f03bf506aa641be890f612a8d626f0c9e37f35fd
skylurk <skylurkauto@gmail.com>
2019-12-13 17:18:16 +0300

    changes

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -179,7 +179,7 @@ export class LandingPage {
               </hgroup>
               <div>
                 <ul class="list--icon list--unstyled">
-                  <li><app-icon name="checkmark"/> Battery powered sensors in the envelope detect any attempts to open or manipulate the package.</li>
+                  <li class="paragraphs"> Battery powered sensors in the envelope detect any attempts to open or manipulate the package.</li>
                   {/**
                    *<li><app-icon name="checkmark"/>Web components run on any platform or device</li>
                   <li><app-icon name="checkmark"/>Build a custom UI library that works across teams and projects</li>
@@ -204,7 +204,7 @@ export class LandingPage {
               </hgroup>
               <div>
                 <ul class="list--icon list--unstyled">
-                  <li><app-icon name="checkmark"/>The sender will receive proof that the package was received intact by the designated recipient.</li>
+                  <li class="paragraphs">The sender will receive proof that the package was received intact by the designated recipient.</li>
                   {/*
                   <li><app-icon name="checkmark"/> Screenshot visual UI diffs</li>
                   <li><app-icon name="checkmark"/> Auto-generate component documentation <br/><span class="small">(including css variables)</span></li>
@@ -215,25 +215,7 @@ export class LandingPage {
           </div>
         </section>
 
-        <section>
-          <div class="container">
-            <div class="two-col">
-              <hgroup>
-                <h2><strong>Limitless Use-Cases</strong></h2>
-              </hgroup>
-              <div>
-                <ul class="list--icon list--unstyled">
-                  <li><app-icon name="checkmark"/>Any time security and time sensitivity are factors, SendSecure.to's technology could be a useful addition to your existing security measures. Here are just some ideas to get you started.
-                  </li>
-                  {/*
-                  <li><app-icon name="checkmark"/> Screenshot visual UI diffs</li>
-                  <li><app-icon name="checkmark"/> Auto-generate component documentation <br/><span class="small">(including css variables)</span></li>
-                  */}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
+
 
         <section>
           <div class="container">
@@ -243,7 +225,8 @@ export class LandingPage {
               </hgroup>
               <div>
                 <ul class="list--icon list--unstyled">
-                  <li><app-icon name="checkmark"/>Any time security and time sensitivity are factors, SendSecure.to's technology could be a useful addition to your existing security measures. Here are just some ideas to get you started.
+                {/**<app-icon name="checkmark"/> */}
+                  <li class="paragraphs">Any time security and time sensitivity are factors, SendSecure.to's technology could be a useful addition to your existing security measures. Here are just some ideas to get you started.
                   </li>
                   {/*
                   <li><app-icon name="checkmark"/> Screenshot visual UI diffs</li>
@@ -263,7 +246,7 @@ export class LandingPage {
                 <h2><strong>Make it yours.</strong> </h2>
               </hgroup>
               <div>
-                <p>Need to enhance security of an existing system or product? We can license use of our technology, either by integration into your existing platform or allowing you to brand and tailor ours as needed. Click the link below and to visit Acuvera, the company behind SendSecure, and find out more.</p>
+                <p class="paragraphs">Need to enhance security of an existing system or product? We can license use of our technology, either by integration into your existing platform or allowing you to brand and tailor ours as needed. Click the link below and to visit Acuvera, the company behind SendSecure, and find out more.</p>
                 {/**<p>Learn how Stencil seamlessly integrates with:</p> */}
 
 
